672ca746d47a1515253be81e6c0757ef07f2ea2a
Dmitry Bespalov <dmitry.bespalov@nordigy.ru>
2017-01-12 18:57:03 +0300

    ConsoleStreamTest: fork usage

--- src/main/scala/com/github/bespalovdn/funcstream/examples/stream/ConsoleStreamTest.scala
+++ src/main/scala/com/github/bespalovdn/funcstream/examples/stream/ConsoleStreamTest.scala
@@ -47,14 +47,15 @@ class ConsoleStreamTest extends FutureExtensions
         def prompt(msg: String): Consumer[Unit] = FConsumer { implicit stream =>
             stream.write(msg) >> success(consume())
         }
-        /*def repeat(c: Consumer[Unit], nTimes: Int): Consumer[Unit] = FConsumer { implicit stream =>
-            if(nTimes > 0) c.apply(stream) >> repeat(c, nTimes - 1).apply(stream)
-            else success(consume())
-        }*/
+//        def repeat(c: Consumer[Unit], nTimes: Int): Consumer[Unit] = FConsumer { implicit stream =>
+//            if(nTimes > 0) c.apply(stream) >> repeat(c, nTimes - 1).apply(stream)
+//            else success(consume())
+//        }
         // build the pipeline and run:
         val result = stream <=> readInt <=> {
             prompt("This is some kind of calculator. Please follow the instructions:") >>
             adder >>
+//            FConsumer.fork(repeat(prompt("FORKED STRING"), 5)) >>
             multiplier >>
             prompt("That's it!")
         }
@@ -73,5 +74,5 @@ class ConsoleStreamTest extends FutureExtensions
 
 object ConsoleStreamTest extends App
 {
-    new ConsoleStreamTest().test1()
+    new ConsoleStreamTest().test2()
 }
